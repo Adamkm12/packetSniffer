0bc3d0a5c215037504bd25fc4b4a6099f675a65f
Adam <Adamkardouchi11@gmail.com>
2026-01-18 23:56:41 +0000

    Base sniffer done

--- packetSniffer.py
+++ packetSniffer.py
@@ -12,6 +12,44 @@ def main():
 
         print('\nEthernet Frame:')
         print(f'Destination: {dest_mac}, Source: {src_mac}, Protocol: {eth_proto}')
+        # IPV4
+        if eth_proto == 8:
+            (version, header_length, ttl, proto, src, target, data) = ipv4_packet(data)
+            print('\t- IPv4 Packet:')
+            print(f'\t\t- Version: {version}, Header Length: {header_length}, TTL: {ttl}')
+            print(f'\t\t- Protocol: {proto}, Source: {src}, Target: {target}')
+
+            # ICMP
+            if proto == 1:
+                icmp_type, code, checksum, data = icmp_packet(data)
+                print('\t\t- ICMP Packet:')
+                print(f'\t\t\t- Type: {icmp_type}, Code: {code}, Checksum: {checksum}')
+                print('\t\t\t- Data:')
+                print(format_multi_line('\t\t\t\t', data))
+
+            # TCP
+            elif proto == 6:
+                (src_port, dest_port, sequence, acknowledgment, flag_urg, flag_ack,
+                 flag_psh, flag_rst, flag_syn, flag_fin, data) = tcp_segment(data)
+                print('\t\t- TCP Segment:')
+                print(f'\t\t\t- Source Port: {src_port}, Destination Port: {dest_port}')
+                print(f'\t\t\t- Sequence: {sequence}, Acknowledgment: {acknowledgment}')
+                print(f'\t\t\t- Flags:')
+                print(f'\t\t\t\t- URG: {flag_urg}, ACK: {flag_ack}, PSH: {flag_psh}, RST: {flag_rst}, SYN: {flag_syn}, FIN: {flag_fin}')
+                print('\t\t\t- Data:')
+                print(format_multi_line('\t\t\t\t', data))
+
+            # UDP
+            elif proto == 17:
+                src_port, dest_port, size, data = udp_segment(data)
+                print('\t\t- UDP Segment:')
+                print(f'\t\t\t- Source Port: {src_port}, Destination Port: {dest_port}, Size: {size}')
+                print('\t\t\t- Data:')
+                print(format_multi_line('\t\t\t\t', data))
+
+            else:
+                print('\t\t- Other IPv4 Protocol:')
+                print(format_multi_line('\t\t\t', data))
         
 
 
@@ -32,5 +70,43 @@ def ipv4_packet(data):
     header_length = (version_header_length & 15) * 4
     ttl, proto, src, target = struct.unpack('! 8x B B 2x 4s 4s', data[:20])
     return version, header_length, ttl, proto, ipv4(src), ipv4(target), data[header_length:]
+
+# FUNCTION TO FORMAT IPV4 ADDRESS
+def ipv4(addr):
+    return '.'.join(map(str, addr))
+
+
+# UNPACK ICMP PACKET
+def icmp_packet(data):
+    icmp_type, code, checksum = struct.unpack('! B B H', data[:4])
+    return icmp_type, code, checksum, data[4:]
+
+# UNPACK TCP PACKET
+def tcp_segment(data):
+    (src_port, dest_port, sequence, acknowledgment, offset_reserved_flags) = struct.unpack('! H H L L H', data[:14])
+    offset = (offset_reserved_flags >> 12) * 4
+    flag_urg = (offset_reserved_flags & 32) >> 5
+    flag_ack = (offset_reserved_flags & 16) >> 4
+    flag_psh = (offset_reserved_flags & 8) >> 3
+    flag_rst = (offset_reserved_flags & 4) >> 2
+    flag_syn = (offset_reserved_flags & 2) >> 1
+    flag_fin = offset_reserved_flags & 1
+    return src_port, dest_port, sequence, acknowledgment, flag_urg, flag_ack, flag_psh, flag_rst, flag_syn, flag_fin, data[offset:]
+
+# UNPACK UDP PACKET
+def udp_segment(data):
+    src_port, dest_port, size = struct.unpack('! H H 2x H', data[:8])
+    return src_port, dest_port, size, data[8:]
+
+# Formats multi-line data
+def format_multi_line(prefix, string, size=80):
+    size -= len(prefix)
+    if isinstance(string, bytes):
+        string = ''.join(r'\x{:02x}'.format(byte) for byte in string)
+    return '\n'.join([prefix + line for line in textwrap.wrap(string, size)])
+
+
+
+
 
 main()
